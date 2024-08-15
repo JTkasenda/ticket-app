9867748448ec5ac1477a6e9c1ae33121ab2ab1de
LordSnow <jeantshimbalanga9@gmail.com>
2024-08-15 01:51:59 +0100

    Trying to solve dependencies

---
+++ lib/base/buttom_nav_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBar();
+}
+
+class _BottomNavBar extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My tickets'),
+        ),
+        body: const Center(
+          child: Text("Ticket Info"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: const Color(0xFF526400),
+          showSelectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Tickets"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ],
+        ));
+  }
+}
